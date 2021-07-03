5c85a5784328c3d2306422c8312ffedcd3ce6462
PowerLichen <dltm135@gmail.com>
2021-07-03 15:13:36 +0900

    Set not found(404) page

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ var url = require('url');
 var app = http.createServer(function (req, res) {
     var _url = req.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
-    if (_url == '/') {
-        title = 'Welcome';
-        _url = '/index.html';
-    };
-    if (_url == '/favicon.ico') {
-        return res.writeHead(404);
-    };
-    res.writeHead(200);
+
+    // Show path without query string
+    // console.log(url.parse(_url,true).pathname)
+
+    // if (_url == '/') {
+    //     title = 'Welcome';
+    //     _url = '/index.html';
+    // };
+    // if (_url == '/favicon.ico') {
+    //     return res.writeHead(404);
+    // };
+
     // comment: just show page
     // console.log(__dirname + _url);
     // res.end(fs.readFileSync(__dirname + _url));
@@ -20,32 +25,37 @@ var app = http.createServer(function (req, res) {
     //node.js data transfer
     // res.end('egoing : ' + _url);
 
-    //URL parse by query string
-    fs.readFile(`data/${title}`, 'utf8', function (err, data) {
-        var description = data;
-        var templete = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${data}</p>
-        </body>
-        </html>
-        `;
-        res.end(templete);
-    });
+    if (pathname == '/') {
+        //URL parse by query string
+        fs.readFile(`data/${title}`, 'utf8', function (err, data) {
+            var description = data;
+            var templete = `
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${data}</p>
+            </body>
+            </html>
+            `;
+            res.writeHead(200);
+            res.end(templete);
+        });
+    } else {
+        res.writeHead(404);
+        res.end('Not Found');
+    };
 
-    
 })
 
 app.listen(3000);
